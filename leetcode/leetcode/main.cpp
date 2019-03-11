70c973ef423f98ac132f7e82c0621296799ed695
ligang <ligang@zepplabs.com>
2019-03-11 22:55:28 +0800

    Initial

---
+++ leetcode/leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by  jueying on 2019/3/11.
+//  Copyright © 2019  jueying. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
