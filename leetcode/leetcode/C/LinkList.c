6354fab83ba1dc3d6afd39f007d9ae534f928ea8
ligang <gang.li@welines.cn>
2019-03-13 21:32:45 +0800

    Add linklist.

---
+++ leetcode/leetcode/C/LinkList.c
@@ -0,0 +1,16 @@
+//
+//  LinkList.c
+//  leetcode
+//
+//  Created by jueying on 2019/3/13.
+//  Copyright © 2019  jueying. All rights reserved.
+//
+
+#include "LinkList.h"
+
+
+struct LinkListNode {
+	LinkListValue value;
+	LinkListNodeRef prev;
+	LinkListNodeRef next;
+};
---
+++ leetcode/leetcode/C/LinkList.h
@@ -0,0 +1,17 @@
+//
+//  LinkList.h
+//  leetcode
+//
+//  Created by jueying on 2019/3/13.
+//  Copyright © 2019  jueying. All rights reserved.
+//
+
+#ifndef LinkList_h
+#define LinkList_h
+
+typedef void *LinkListValue;
+
+typedef struct LinkListNode *LinkListNodeRef;
+
+
+#endif /* LinkList_h */
