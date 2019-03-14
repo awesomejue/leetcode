ab74b811892ee060214cc4ee4665e0e6fb1f2229
jueying <jueying2014@foxmail.com>
2019-03-14 23:49:58 +0800

    Refine LinkList

--- leetcode/leetcode/C/LinkList.c
+++ leetcode/leetcode/C/LinkList.c
@@ -7,10 +7,43 @@
 //
 
 #include "LinkList.h"
-
+#include "CHeader.h"
 
 struct LinkListNode {
 	LinkListValue value;
 	LinkListNodeRef prev;
 	LinkListNodeRef next;
 };
+
+unsigned int LKListLength(LinkListNodeRef head)
+{
+	unsigned length = 0;
+	while (head->next != NULL) {
+		length++;
+		head = head->next;
+	}
+	
+	return length;
+}
+
+LinkListNodeRef LKNodeAtIndex(LinkListNodeRef head, unsigned int index)
+{
+	for (int i = 0; i < index; i++) {
+		if (head == NULL) {
+			return NULL;
+		}
+		head = head->next;
+	}
+	
+	return head;
+}
+
+LinkListValue LKValueAtIndex(LinkListNodeRef head, unsigned int index)
+{
+	return LKValueForNode(LKNodeAtIndex(head, index));
+}
+
+LinkListValue LKValueForNode(LinkListNodeRef node)
+{
+	return node->value;
+}
--- leetcode/leetcode/C/LinkList.h
+++ leetcode/leetcode/C/LinkList.h
@@ -9,9 +9,27 @@
 #ifndef LinkList_h
 #define LinkList_h
 
+#define LIST_NULL ((void *) 0)
+
 typedef void *LinkListValue;
 
 typedef struct LinkListNode *LinkListNodeRef;
+
+LinkListNodeRef LKAppendValue(LinkListNodeRef head, LinkListValue value);
+
+LinkListNodeRef LKPrependValue(LinkListNodeRef head, LinkListValue value);
+
+void LKFreeList(LinkListNodeRef head);
+
+LinkListValue LKValueForNode(LinkListNodeRef node);
+
+void LKSetValueForNode(LinkListNodeRef node, LinkListValue value);
+
+unsigned int LKListLength(LinkListNodeRef head);
+
+LinkListNodeRef LKNodeAtIndex(LinkListNodeRef head, unsigned int index);
+
+LinkListValue LKValueAtIndex(LinkListNodeRef head, unsigned int index);
 
 
 #endif /* LinkList_h */
